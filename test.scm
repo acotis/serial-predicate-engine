f919aba87889625a620d8e90ff7089a74dd53177
Evan Rysdam <evan@rysdam.org>
2018-08-07 06:37:06 -0400

    Changed test macro to a function in the Scheme version.

--- test.scm
+++ test.scm
@@ -1,31 +1,20 @@
 
 
-
-(define-syntax perform
-  (syntax-rules ()
-    ((perform call)
-     (begin (format #t "Call:   ~a~%" 'call)
-            (format #t "Result: ~a~%" call)))))
+(load "serial_expansion.scm")
 
 
-(define-syntax test
-  (syntax-rules ()
-    ((test call expected display-anyway)
-     (let ((result call))
+(define (test call expected #!optional (display-anyway #f))
+  (let ((result (eval call)))
 
-       (if (equal? result expected)
-           (begin (when display-anyway
-                        (format #t "Test:     ~a~%" 'call)
-                        (format #t "Result:   ~a~%" result))
-                  #t)
-             
-           (begin (format #t "Test:     ~a~%" 'call)
-                  (format #t "Expected: ~a~%" 'expected)
-                  (format #t "Result:   ~a~%" result)
-                  #f))))
+    (if (equal? expected result)
+        (begin (when display-anyway
+                     (format #t "Test:     ~a~%" call)
+                     (format #t "Result:   ~a~%" result))
+               #t)
 
-    ((test call expected)
-     (test call expected #f))))
+        (begin (format #t "Test:     ~a~%" call)
+               (format #t "Expected: ~a~%" expected)
+               (format #t "Result:   ~a~%" result)))))
 
 
-(test (+ 3 5) 7)
+(test '(is-filled-slot 'c) #t)
