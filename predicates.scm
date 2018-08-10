98b9ea8f5d3e39dca761a66a69298dcd19a5bdfc
Evan Rysdam <evan@rysdam.org>
2018-08-10 05:29:25 -0400

    Added (expand-binary) and tests.

--- predicates.scm
+++ predicates.scm
@@ -25,6 +25,14 @@
       (fun (car ls) (fold fun (cdr ls)))))
 
 
+;; Return the index of first e in ls such that (fun e) is true
+;; (Assumes that such an e exists)
+(define (find-first fun ls)
+  (if (fun (car ls))
+      0
+      (+ 1 (find-first fun (cdr ls)))))
+
+
 ;; Create a simple predicate given its name and typelist
 ;; (The predicate function assumes that the correct number
 ;;  of args are passed to it)
@@ -54,24 +62,38 @@
   
 
 ;; Expand a two-part predicate into one predicate.
+;; Assumes head has an abstraction place.
 ;; Examples:
 ;;   (dua c 0) (mai c c) = (dua c (mai c c))
 ;;   (leo c 1) (mai c c) = (leo c (mai jado c))
 ;;   (cheo c 2) (mai c c) = (cheo c (mai jado jado))
 
 (define (expand-binary head tail)
-  (let ((mix (lambda (ls)
-               (append (cddr ls)
-                       (list (cadr ls)))))
-        (fix (lambda (ls)
-               (append (list (car head))
-                       (take-right ls 1)
-                       (drop-right ls 1)))))
-    (fix
-     (replace-first (mix head)
-                    number?
-                    (lambda (s)
-                      (fill-slots tail (make-list s 'jado)))))))
+  (let* ((hpred (car head))
+         (htypes (cdr head))
+         
+         (slot ((lambda (k)
+                  (if (= k (- (length htypes) 1)) 0 (+ k 1)))
+                (find-first number?
+                            (append (cdr htypes)
+                                    (list (car htypes))))))
+         
+         (arity (list-ref htypes slot))
+         (new-tail (jado-ify tail arity))
+
+         (tpred (car new-tail))
+         (ttypes (cdr new-tail))
+         (tcount (length ttypes))
+
+         (etypes (append (take htypes slot)
+                         ttypes
+                         (drop htypes (+ slot 1)))))    
+    (cons (lambda (args)
+            (hpred (append
+                    (take args slot)
+                    (list (tpred (take (drop args slot) tcount)))
+                    (drop args (+ slot tcount)))))
+          etypes)))
 
 
 ;; Expand poly-predicates
--- test.scm
+++ test.scm
@@ -27,12 +27,15 @@
      (test call expected #f))))
 
 
-(let ((show-gcf #t)  ;; Show (gcf) "get canonic form" tests
-      (show-jdf #t)  ;; Show (jado-ify) tests
+(let ((show-gcf #f)  ;; Show (gcf) "get canonic form" tests
+      (show-jdf #f)  ;; Show (jado-ify) tests
+      (show-eb #t)   ;; Show (expand-binary) tests
 
+      (gi-1 (make-simple-predicate "gi" '(0)))
       (mai-2 (make-simple-predicate "mai" '(c c)))
       (dua-2 (make-simple-predicate "dua" '(c 0)))
-      (soq-3 (make-simple-predicate "soq" '(1 c 1)))
+      (leo-2 (make-simple-predicate "leo" '(c 1)))
+      (soq-3 (make-simple-predicate "soq" '(c 1 c)))
       (jai-0 (make-simple-predicate "jai" '())))
       
   (if (and
@@ -57,9 +60,31 @@
              '("soq" A B C)
              show-jdf)
        (test (cdr (jado-ify soq-3 1))
-             '(c 1)
+             '(1 c)
              show-jdf)
-       
+
+       ;; expand-binary
+       (let ((dua-mai (expand-binary dua-2 mai-2))
+             (leo-mai (expand-binary leo-2 mai-2))
+             (soq-dua (expand-binary soq-3 dua-2))
+             (soq-gi  (expand-binary soq-3 gi-1))
+             (dua-jai (expand-binary dua-2 jai-0)))
+         (and 
+          (test (gcf dua-mai) '("dua" A ("mai" B C))     show-eb)
+          (test (cdr dua-mai) '(c c c)                   show-eb)
+          
+          (test (gcf leo-mai) '("leo" A ("mai" jado B))  show-eb)
+          (test (cdr leo-mai) '(c c)                     show-eb)
+          
+          (test (gcf soq-dua) '("soq" A ("dua" jado B) C)show-eb)
+          (test (cdr soq-dua) '(c 0 c)                   show-eb)
+
+          (test (gcf soq-gi)  '("soq" A ("gi" jado) B)   show-eb)
+          (test (cdr soq-gi)  '(c c)                     show-eb)
+          
+          (test (gcf dua-jai) '("dua" A ("jai"))         show-eb)
+          (test (cdr dua-jai) '(c)                       show-eb)
+         ))
        )
       
       (format #t "All tests passed.~%")
