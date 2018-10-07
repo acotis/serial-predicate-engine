510010bda85008d08510aa82032c237079a0b808
Evan Rysdam <evan@rysdam.org>
2018-10-07 12:03:10 -0400

    Implemented a basic web server.

--- web/webservice.scm
+++ web/webservice.scm
@@ -2,4 +2,30 @@
 !#
 
 
-(use-modules (web server))
+(use-modules (web server)
+             (web request)
+             (web response)
+             (web uri))
+
+(load "../api/api.scm") ;; Serial predicate engine API impl
+
+
+(define (append-lines lines)
+  (if (= 0 (length lines))
+      ""
+      (string-append (car lines)
+                     "\n"
+                     (append-lines (cdr lines)))))
+
+(define (request-path-components request)
+  (split-and-decode-uri-path (uri-path (request-uri request))))
+
+(define (hello-hacker-handler request request-body)
+  (let ((input (request-path-components request)))
+    (values '((content-type . (text/plain)))
+            (append-lines (api-parse (car input))))))
+
+
+(api-preload)
+
+(run-server hello-hacker-handler)
