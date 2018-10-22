027596f1bc45823557aced80ddc29b3fe51a0d13
Evan Rysdam <evan@rysdam.org>
2018-10-22 02:02:37 -0400

    Got a Flask website to remember the last ten queries.

--- web/website.py
+++ web/website.py
@@ -23,9 +23,51 @@ def helloDefault():
         value = '0'
     value = str(int(value) + 1)
 
-    resp = make_response(render_template('default.html', value=value))
+    resp = make_response(render_template('default.html',
+                                         value=value))
     resp.set_cookie('key', value)
     return resp
+
+
+## PARSE PAGE HELPERS
+
+# A pair of functions for turning the (string) data stored in
+# the recent_parses cookies into a list and back
+
+def getQueries(recent):
+    if recent is None:
+        return []
+    return recent.split(";")
+
+def getRecent(queries):
+    length = len(queries)
+
+    if length > 10:
+        queries = queries[length-10:]
+
+    ret = ""
+    for query in queries:
+        ret += ";"
+        ret += query
+
+    return ret[1:]
     
+@app.route("/parse/")
+def parse():
+    query = request.args.get('input')
+
+    recent = request.cookies.get('recent_parses')
+    queries = getQueries(recent)
+    queries.append(query)
+    recent = getRecent(queries)
+
+    queries.reverse()
+    resp = make_response(render_template('recent.html',
+                                         queries=queries))
+
+    resp.set_cookie('recent_parses', recent)
+    return resp
+    
+
 app.debug = True
 app.run()
